aed3961179d40573936e9c8c3bfbb94e8b76b437
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-21 20:58:50 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,21 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+    # binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    # 10 * "s" + string
+    string[0] * 10 + string
   else
-    string
+    return string
   end
 end
+
+# def snake_it_up(string)
+#     # binding.pry
+#   if string[0] == "s"
+#     10 * string[0] + string
+#   else
+#     string
+#   end
+# end
