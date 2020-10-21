40267e2d80135de9bb34f9b0567ffa264e18b7ae
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-21 21:03:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,6 @@
+# don't forget to add: require 'pry'
+
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -5,19 +8,25 @@ end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  puts "Captain's Log, star date #{star_date}."
 end
 
-def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
-end
+# def crew
+#   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+# end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
-end
+# def greet_crew (crew)
+#   crew.each {|crew_member| "Hello #{crew_member}."}
+# end
+
+# def greet_crew(crew)
+#    crew.each do |crew_mem|
+#     puts "hello, #{crew_mem}"
+#    end
+# end
 
 def engage
+    state_log(star_date)
   date = generate_star_date
-  state_log(date)
   greet_crew(crew)
 end
