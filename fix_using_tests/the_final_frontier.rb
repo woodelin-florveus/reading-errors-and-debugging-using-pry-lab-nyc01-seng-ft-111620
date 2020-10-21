773ffa4f8fdcfea0cd5eab1b0434083f923a17ca
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-21 21:07:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -11,13 +11,13 @@ def state_log(star_date)
   puts "Captain's Log, star date #{star_date}."
 end
 
-# def crew
-#   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
-# end
-#
-# def greet_crew(crew)
-#   crew.map {|crew_member| "Hello #{crew_member}."}
-# end
+def crew
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+end
+
+def greet_crew(crew)
+  crew.map {|crew_member| "Hello #{crew_member}."}
+end
 
 def engage
   date = generate_star_date
